3352d99aad3e2764ad1e7cf19f96a8164a1467a5
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2023-05-15 19:10:47 +0200

    fix: javascript debugger

--- lua/dap/javascript.lua
+++ lua/dap/javascript.lua
@@ -20,14 +20,16 @@ end
 dapvscodejs.setup({
   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
   -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+  debugger_path = lvim.lazy.opts.root .. "/vscode-js-debug/", -- Path to vscode-js-debug installation.
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
   -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 })
-
-for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+-- lvim.lazy.opts.root
+-- lua print(lvim.lazy.opts.root)
+for _, language in ipairs({ "typescriptreact", "javascriptreact" }) do
   dap.configurations[language] = {
     {
       type = "pwa-chrome",
@@ -63,6 +65,12 @@ for _, language in ipairs({ "typescript", "javascript" }) do
       cwd = "${workspaceFolder}",
       console = "integratedTerminal",
       internalConsoleOptions = "neverOpen",
+    },
+    {
+      type = "pwa-chrome",
+      name = "Debug in Chrome",
+      request = "launch",
+      url = "http://localhost:3000",
     },
   }
 end
