acc026071dd8e2c138681baeccc42a43774dc0b4
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2023-06-12 01:24:28 +0200

    add: copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,4 +45,15 @@ lvim.plugins = {
   },
   { "mfussenegger/nvim-dap-python" },
   { "tpope/vim-surround" },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()   -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  }
 }
